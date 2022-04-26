2ae1d46efa20b84adadb434c7e0fced4aefae022
Sandra ter Maat <sandra@tweedegolf.com>
2022-04-26 15:44:09 +0200

    added datarates for ccm

--- nrf5340-net-hal/src/lib.rs
+++ nrf5340-net-hal/src/lib.rs
@@ -8,16 +8,11 @@ pub use nrf_hal_common::*;
 pub mod prelude {
     pub use crate::hal::prelude::*;
     pub use nrf_hal_common::prelude::*;
-
     pub use crate::time::U32Ext;
 }
-
-// TODO uitzoeken welke ik nodig heb voor de gpio?
-
 pub use crate::clocks::Clocks;
 pub use crate::delay::Delay;
 pub use crate::rtc::Rtc;
-// pub use crate::saadc::Saadc;
 pub use crate::spim::Spim;
 pub use crate::timer::Timer;
 pub use crate::twim::Twim;
